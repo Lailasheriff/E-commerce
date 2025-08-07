c3af6def5ff21b8bbb82d802bff4ac0001317bb2
Lailasheriff <lailakassem03@gmail.com>
2025-08-07 23:54:35 +0300

    Add handle validation

--- src/main/java/com/project/ecommerce/dto/ReviewRequest.java
+++ src/main/java/com/project/ecommerce/dto/ReviewRequest.java
@@ -2,6 +2,7 @@ package com.project.ecommerce.dto;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,11 +12,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ReviewRequest {
 
+    @NotNull
     private long productId;
 
     @Min(value = 1, message = "Rating must be at least 1")
     @Max(value = 5, message = "Rating must be at most 5")
     private int rating;
 
+    @NotNull
     private String comment;
 }
