97d204b30eb7090588804a9a95253ab6e47e6dd8
shahdsedek <shahd.elsedek@gmail.com>
2025-08-10 13:24:28 +0300

    seller with exceptions 1:24

--- src/main/java/com/project/ecommerce/service/SellerOrderServiceImpl.java
+++ src/main/java/com/project/ecommerce/service/SellerOrderServiceImpl.java
@@ -11,12 +11,11 @@ import com.project.ecommerce.repository.OrderRepository;
 import com.project.ecommerce.repository.ProductRepository;
 import com.project.ecommerce.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
+
 
 
 @Service
