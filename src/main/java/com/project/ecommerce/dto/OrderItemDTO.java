d9f3dd4ce4e5bb544283a27eb403bb4074ee8a2f
Lailasheriff <lailakassem03@gmail.com>
2025-08-10 14:53:32 +0300

    merging all

---
+++ src/main/java/com/project/ecommerce/dto/OrderItemDTO.java
@@ -0,0 +1,19 @@
+package com.project.ecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class OrderItemDTO {
+
+    private String productName;
+    private int quantity;
+    private BigDecimal price;
+    private Long productId;
+
+}
